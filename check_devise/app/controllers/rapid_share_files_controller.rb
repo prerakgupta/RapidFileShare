2d4227b622e41bb31b460e126859827b258f8953
Prerak Gupta <prerakgupta@Preraks-MacBook-Pro.local>
2015-07-10 20:05:10 +0530

    Updated as per review

--- check_devise/app/controllers/rapid_share_files_controller.rb
+++ check_devise/app/controllers/rapid_share_files_controller.rb
@@ -1,19 +1,26 @@
 class RapidShareFilesController < ApplicationController
 
+      before_filter :check_file_availability, :only => [:show, :destroy]
+      before_filter :check_authencity_of_user, :only => :destroy
+
+
       def index
-          @files = RapidShareFile.where("user_id = ?", current_user.id)
+          @files = RapidShareFile.all;
       end
 
       def create
-          u = RapidShareFile.new
-	  u.user_id = current_user.id
-          u.file = params[:file]
-          if u.save
-             flash[:notice] = "The file was saved successfully"
+          if user_signed_in?
+            u = RapidShareFile.new
+	          u.user_id = current_user.id
+            u.file = params[:file]
+              if !u.save
+                flash[:error] = "There was an error in saving the file. Please try again."
+              end
+            redirect_to rapid_share_files_path
           else
-             flash[:error] = "There was an error in saving the file. Please try again."
+            redirect_to destroy_user_session_path
           end
-          redirect_to rapid_share_files_path
+
       end
 
       def show
@@ -27,4 +34,19 @@ class RapidShareFilesController < ApplicationController
           redirect_to rapid_share_files_path
       end
 
+      protected
+        def check_authencity_of_user
+            @file = RapidShareFile.find(params[:id]);
+            if current_user.id!=@file.user_id
+              flash[:error] = "You are not authorised to delete this."
+              redirect_to rapid_share_files_path
+            end
+        end
+
+        def check_file_availability
+          if !RapidShareFile.exists?(params[:id])
+              flash[:error] = "The file with the given id does not exist.";
+              redirect_to rapid_share_files_path
+          end
+        end
 end
--- check_devise/app/controllers/users_controller.rb
+++ check_devise/app/controllers/users_controller.rb
@@ -1,19 +1,2 @@
 class UsersController < ApplicationController
-
-      def index
-      	  if user_signed_in?
-		render plain: current_user.id.inspect
-	  else
-		render plain: "index not signed in"
-	  end			
-      end
-
-      def new
-      	  render plain: "new"
-      end
-
-      def create
-      	  render plain: "create"
-      end	  	  
-
 end
